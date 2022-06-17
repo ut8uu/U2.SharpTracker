e9c2ec14fea8979ff7e44232c6ad026b31cf8076
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-06-18 01:11:32 +0300

    Added TrackerLoaded and related stuff

---
+++ Sources/U2.SharpTracker.Core/Classes/TrackerLoader.cs
@@ -0,0 +1,48 @@
+﻿/* 
+ * This file is part of the U2.SharpTracker distribution
+ * (https://github.com/ut8uu/U2.SharpTracker).
+ * 
+ * Copyright (c) 2022 Sergey Usmanov.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U2.SharpTracker.Core;
+
+public delegate void PageDownloadFinishedEventHandler(object sender, PageLoadResult result);
+
+public sealed class TrackerLoader
+{
+    private PageLoadRequest _request;
+
+    public string Title { get; set; }
+    public int Index { get; set; }
+
+    public event PageDownloadFinishedEventHandler PageDownloadFinished;
+
+    private void OnPageDownloadFinished(PageLoadResult result)
+    {
+        PageDownloadFinished?.Invoke(this, result);
+    }
+
+    public void LoadPage(PageLoadRequest request)
+    {
+        _request = request;
+    }
+}
