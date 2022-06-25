811b711dce09830ba3fea71fa92c2a57ef700ac8
Sergey Usmanov <sergey.usmanov@materialise.lkiev.ua>
2022-06-25 20:20:48 +0300

    Added Web and Svc projects

--- Sources/U2.SharpTracker.Utils/Runner.cs
+++ Sources/U2.SharpTracker.Utils/Runner.cs
@@ -8,19 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using U2.SharpTracker.Core;
 
-namespace U2.SharpTracker.Utils
+namespace U2.SharpTracker.Utils;
+
+public class Runner
 {
-    public class Runner
+    private Task _task;
+    private CancellationTokenSource _cancellationTokenSource;
+
+    public void Run()
     {
-        private Task _task;
-        private CancellationTokenSource _cancellationTokenSource;
+        _cancellationTokenSource = new CancellationTokenSource();
+        var startKey = ConsoleKey.D1;
 
-        public void Run()
-        {
-            _cancellationTokenSource = new CancellationTokenSource();
-            var startKey = ConsoleKey.D1;
-
-            var mainMenu = new List<ConsoleManagementElement>
+        var mainMenu = new List<ConsoleManagementElement>
             {
                 new()
                 {
@@ -31,68 +31,68 @@ namespace U2.SharpTracker.Utils
 
             };
 
-            ManageFlow(mainMenu);
-        }
+        ManageFlow(mainMenu);
+    }
 
-        public void Stop()
+    public void Stop()
+    {
+        _cancellationTokenSource.Cancel();
+    }
+
+    void Log(string message)
+    {
+        LogManager.GetLogger(typeof(Runner)).Info(message);
+    }
+
+    void ManageFlow(List<ConsoleManagementElement> input)
+    {
+        var output = new StringBuilder();
+        foreach (var el in input)
         {
-            _cancellationTokenSource.Cancel();
-        }
-
-        void Log(string message)
-        {
-            LogManager.GetLogger(typeof(Runner)).Info(message);
-        }
-
-        void ManageFlow(List<ConsoleManagementElement> input)
-        {
-            var output = new StringBuilder();
-            foreach (var el in input)
-            {
-                output.Append($"{el.Key}. {el.Title}");
-                output.AppendLine();
-            }
-
+            output.Append($"{el.Key}. {el.Title}");
             output.AppendLine();
-            output.AppendLine("X. Exit");
-
-            while (true)
-            {
-                Console.Clear();
-                Console.WriteLine(output);
-
-                var key = Console.ReadKey();
-                if (input.Any(o => o.Key == key.Key))
-                {
-                    var func = input.FirstOrDefault(o => o.Key == key.Key);
-                    if (func != null)
-                    {
-                        Console.Clear();
-                        Console.WriteLine(func.Title);
-                        Console.WriteLine("---------------------------------------");
-
-                        func.Function(func.FunctionParameters);
-
-                        Console.WriteLine("---------------------------------------");
-                        Console.WriteLine("Press any key to continue...");
-                        Console.ReadKey();
-                    }
-                }
-                else if (key.Key == ConsoleKey.X)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Unrecognized input");
-                    Console.WriteLine("");
-                }
-            }
         }
 
-        bool RuTrackerFunctions(object[] parameters)
+        output.AppendLine();
+        output.AppendLine("X. Exit");
+
+        while (true)
         {
-            var rutrackerMenu = new List<ConsoleManagementElement>
+            Console.Clear();
+            Console.WriteLine(output);
+
+            var key = Console.ReadKey();
+            if (input.Any(o => o.Key == key.Key))
+            {
+                var func = input.FirstOrDefault(o => o.Key == key.Key);
+                if (func != null)
+                {
+                    Console.Clear();
+                    Console.WriteLine(func.Title);
+                    Console.WriteLine("---------------------------------------");
+
+                    func.Function(func.FunctionParameters);
+
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Press any key to continue...");
+                    Console.ReadKey();
+                }
+            }
+            else if (key.Key == ConsoleKey.X)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Unrecognized input");
+                Console.WriteLine("");
+            }
+        }
+    }
+
+    bool RuTrackerFunctions(object[] parameters)
+    {
+        var rutrackerMenu = new List<ConsoleManagementElement>
     {
         new()
         {
@@ -102,79 +102,83 @@ namespace U2.SharpTracker.Utils
         },
 
     };
-            ManageFlow(rutrackerMenu);
+        ManageFlow(rutrackerMenu);
 
-            return true;
-        }
+        return true;
+    }
 
-        void StrategyOnUserInputRequired(object sender, UserInputRequiredEventArgs eventArgs)
+    void StrategyOnUserInputRequired(object sender, UserInputRequiredEventArgs eventArgs)
+    {
+        Console.WriteLine("------------------------------------");
+        Console.Write(eventArgs.MessageToUser);
+        eventArgs.UserInput = "84";//Console.ReadLine();
+        Console.WriteLine("------------------------------------");
+    }
+
+    void StrategyOnProgressReported(object sender, ProgressReportedEventArgs eventArgs)
+    {
+        Console.WriteLine($"{eventArgs.Progress}% {eventArgs.Text}");
+    }
+
+    void StrategyOnInternetResourceContentRequired(object sender, InternetResourceContentRequiredEventArgs eventArgs)
+    {
+        var task = Task.Run(async () =>
         {
-            Console.WriteLine("------------------------------------");
-            Console.Write(eventArgs.MessageToUser);
-            eventArgs.UserInput = "84";//Console.ReadLine();
-            Console.WriteLine("------------------------------------");
-        }
+            eventArgs.ResourceContent = await DownloadUrlAsync(eventArgs.UrlInfo);
+            eventArgs.UrlInfo.UrlLoadStatusCode = UrlLoadStatusCode.Success;
+            eventArgs.UrlInfo.UrlLoadState = UrlLoadState.Loaded;
+        });
+        task.Wait();
+    }
 
-        void StrategyOnProgressReported(object sender, ProgressReportedEventArgs eventArgs)
+    async Task<string> DownloadUrlAsync(UrlInfo url)
+    {
+        try
         {
-            Console.WriteLine($"{eventArgs.Progress}% {eventArgs.Text}");
+            var client = new HttpClient();
+            var response = await client.GetAsync(url.Url);
+            var content = await response.Content.ReadAsByteArrayAsync();
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var win1251 = Encoding.GetEncoding("windows-1251");
+            var responseString = win1251.GetString(content, 0, content.Length);
+            return responseString;
         }
-
-        void StrategyOnInternetResourceContentRequired(object sender, InternetResourceContentRequiredEventArgs eventArgs)
+        catch (Exception ex)
         {
-            var task = Task.Run(async () =>
-            {
-                eventArgs.ResourceContent = await DownloadUrlAsync(eventArgs.UrlInfo);
-                eventArgs.UrlInfo.UrlLoadStatusCode = UrlLoadStatusCode.Success;
-                eventArgs.UrlInfo.UrlLoadState = UrlLoadState.Loaded;
-            });
-            task.Wait();
+            throw;
         }
+    }
 
-        async Task<string> DownloadUrlAsync(UrlInfo url)
+    bool RTPerBranchStrategy(object[] parameters)
+    {
+        _task = Task.Run(() =>
+            RTPerBranchStrategyAsync(null),
+            _cancellationTokenSource.Token);
+
+        return true;
+    }
+
+    async Task<bool> RTPerBranchStrategyAsync(object[] parameters)
+    {
+        var dbDirectory = FileSystemHelper.GetDatabaseFolderPath();
+        var runner = new MongoDBRunner(dbDirectory);
+        runner.Start();
+
+        var settings = new DatabaseSettings
         {
-            try
-            {
-                var client = new HttpClient();
-                var response = await client.GetAsync(url.Url);
-                var content = await response.Content.ReadAsByteArrayAsync();
-                Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-                var win1251 = Encoding.GetEncoding("windows-1251");
-                var responseString = win1251.GetString(content, 0, content.Length);
-                return responseString;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
+            ConnectionString = UtilsAppSettings.Default.ConnectionString,
+            DatabaseName = "RuTracker",
+        };
+        var database = MongoDatabaseHelper.CreateDatabase(settings);
+        var strategy = new RTPerBranchStrategy(database);
 
-        bool RTPerBranchStrategy(object[] parameters)
-        {
-            _task = Task.Run(() => 
-                RTPerBranchStrategyAsync(null),
-                _cancellationTokenSource.Token);
+        strategy.UserInputRequired += StrategyOnUserInputRequired;
+        strategy.ProgressReported += StrategyOnProgressReported;
+        strategy.InternetResourceContentRequired += StrategyOnInternetResourceContentRequired;
+        await strategy.StartAsync();
 
-            return true;
-        }
+        runner.Stop();
 
-        async Task<bool> RTPerBranchStrategyAsync(object[] parameters)
-        {
-            var dbDirectory = FileSystemHelper.GetDatabaseFolderPath();
-            var runner = new MongoDBRunner(dbDirectory);
-            runner.Start();
-
-            var database = runner.CreateDatabase("RuTracker");
-            var strategy = new RTPerBranchStrategy(database);
-
-            strategy.UserInputRequired += StrategyOnUserInputRequired;
-            strategy.ProgressReported += StrategyOnProgressReported;
-            strategy.InternetResourceContentRequired += StrategyOnInternetResourceContentRequired;
-            await strategy.StartAsync();
-
-            runner.Stop();
-
-            return true;
-        }
+        return true;
     }
 }
---
+++ Sources/U2.SharpTracker.Web/Models/TrackerBranch.cs
@@ -0,0 +1,32 @@
+﻿/* 
+ * This file is part of the U2.SharpTracker distribution
+ * (https://github.com/ut8uu/U2.SharpTracker).
+ * 
+ * Copyright (c) 2022 Sergey Usmanov.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using U2.SharpTracker.Core;
+
+namespace U2.SharpTracker.Web.Models;
+
+public class TrackerBranch
+{
+    public IEnumerable<TrackerBranch> Branches { get; set; }
+    public IEnumerable<TrackerTopic> Topics { get; set; }
+
+    public string Title { get; set; }
+    public string Url { get; set; }
+    public UrlLoadStatusCode StatusCode { get; set; }
+}
